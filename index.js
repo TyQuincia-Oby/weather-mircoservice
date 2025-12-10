1621123f85ed28c20473d36a6bcb1740ce42d80f
TyQuincia Oby <tyquinciaoby@gmail.com>
2025-12-09 20:06:57 -0600

    Step 3 Create a Visual Crossing Account and Step 4 Test the API with Postman [complete]

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ app.get('/', (req, res) => {
     res.json({
        message: 'Hello from home route'
     })
+
+    console.log(req.headers)
 });
+
+//fetch Baton Rouge,LA Weather
+app.get('/')
 
 app.listen(PORT, () => {
     console.log(`Weather Microservice running on port ${PORT}`)
